258d31ee5776c5d442abb1211fa8be3b4b4a9411
Hanhan <764932129@qq.com>
2024-04-16 18:02:43 +0800

    feat: Scheduler

---
+++ src/main/java/me/xiaoying/livegetauthorize/core/scheduler/XiaoYingScheduler.java
@@ -0,0 +1,17 @@
+package me.xiaoying.livegetauthorize.core.scheduler;
+
+import me.xiaoying.livegetauthorize.core.plugin.JavaPlugin;
+
+public interface XiaoYingScheduler {
+    void cancelTask(int task);
+
+    void runTask(JavaPlugin plugin, Runnable runnable);
+
+    int scheduleSyncDelayedTask(JavaPlugin plugin, Runnable runnable);
+    int scheduleSyncDelayedTask(JavaPlugin plugin, Runnable runnable, long delay);
+    int scheduleSyncRepeatingTask(JavaPlugin plugin, Runnable runnable, long delay, long period);
+
+    int scheduleAsyncDelayedTask(JavaPlugin plugin, Runnable runnable);
+    int scheduleAsyncDelayedTask(JavaPlugin plugin, Runnable runnable, long delay);
+    int scheduleAsyncRepeatingTask(JavaPlugin plugin, Runnable runnable, long delay, long period);
+}
