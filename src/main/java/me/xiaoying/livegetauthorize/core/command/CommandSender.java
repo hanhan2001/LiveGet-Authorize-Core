f6c5fc00fd40e3ea65557d91326764a73daca00d
Hanhan <764932129@qq.com>
2024-04-26 17:48:00 +0800

    feat: make new method of "getName" for CommandSender

--- src/main/java/me/xiaoying/livegetauthorize/core/command/CommandSender.java
+++ src/main/java/me/xiaoying/livegetauthorize/core/command/CommandSender.java
@@ -4,5 +4,7 @@ package me.xiaoying.livegetauthorize.core.command;
  * Command Sender
  */
 public interface CommandSender {
+    String getName();
+
     boolean hasPermission(String permission);
 }
